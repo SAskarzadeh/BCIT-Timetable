7567c0aba016caf3855cd9281a497f731fd604e8
Sajjad Askarzadeh <askarzadeh@gmail.com>
2020-03-21 00:15:11 -0700

    Listview is working

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -20,19 +22,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import javax.xml.namespace.QName;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextViewResult;
     private RequestQueue mQueue;
     private Object JsonObjectRequest;
-
+    private ListView mlistView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mlistView = findViewById(R.id.listView);
         mTextViewResult = findViewById(R.id.tvResult);
         Button buttonParse = findViewById(R.id.btnParse);
 
@@ -51,25 +55,21 @@ public class MainActivity extends AppCompatActivity {
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
-           Log.d("Response",response.toString());
-                for(int i = 0; i < response.length(); i++){
-                    JSONObject jresponse = null;
-                    try {
-                        jresponse = response.getJSONObject(i);
-                        Log.d("response2", String.valueOf(jresponse));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    String fullname = null;
-                    try {
-                        fullname = jresponse.getString("name");
-                        Log.d("response3",fullname);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    mTextViewResult.append(fullname +"\n\n");
-                }
 
+                ArrayList<String> arrayList = new ArrayList<String>();
+
+                for(int i = 0; i < response.length(); i++) {
+                    try {
+                        arrayList.add(response.getJSONObject(i).getString("name"));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(arrayList);
+                ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+                System.out.println(arrayAdapter);
+                mlistView.setAdapter(arrayAdapter);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -79,5 +79,37 @@ public class MainActivity extends AppCompatActivity {
         });
         mQueue.add(request);
     }
+    /*
 
+
+    private void getInstructorSchedule(int idInstructor){
+        String url = "https://timetables.bcitsitecentre.ca/api/Timetable/Instructor?instructorID="+idInstructor+"&termSchoolID=77";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                ArrayList<String> arrayList = new ArrayList<String>();
+
+                for(int i = 0; i < response.length(); i++) {
+                    try {
+                        arrayList.add(response.getJSONObject(i).getString("name"));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(arrayList);
+                ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,arrayList);
+                System.out.println(arrayAdapter);
+                mlistView.setAdapter(arrayAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error Response",error.toString());
+            }
+        });
+        mQueue.add(request);
+    }
+     */
 }
